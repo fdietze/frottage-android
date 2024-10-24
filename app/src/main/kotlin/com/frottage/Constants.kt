979212def655bdc412b91c660bcb46089a5691b3
Felix Dietze <github@felx.me>
2024-10-24 11:02:54 +0400

    default lockscreen url to frottage

--- app/src/main/kotlin/com/frottage/Constants.kt
+++ app/src/main/kotlin/com/frottage/Constants.kt
@@ -5,7 +5,10 @@ import java.util.TimeZone
 object Constants {
     val WALLPAPER_UPDATE_HOURS_UTC = listOf(1, 7, 13, 19)
     val UTC_TIMEZONE: TimeZone = TimeZone.getTimeZone("UTC")
-    const val DEFAULT_LOCK_SCREEN_WALLPAPER_URL: String = "https://unsplash.it/900/1900/?random"
+
+    // const val DEFAULT_LOCK_SCREEN_WALLPAPER_URL: String = "https://unsplash.it/900/1900/?random"
+    const val DEFAULT_LOCK_SCREEN_WALLPAPER_URL: String = "https://fdietze.github.io/frottage/wallpapers/wallpaper-mobile-latest.jpg"
     const val DEFAULT_HOME_SCREEN_WALLPAPER_URL: String = "https://fdietze.github.io/frottage/wallpapers/wallpaper-mobile-homescreen-latest.jpg"
+
     const val PROMPT_URL: String = "https://fdietze.github.io/frottage/wallpapers/mobile.json"
 }
